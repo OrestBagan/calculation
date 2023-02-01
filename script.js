e058fc3218f7495d223140630eb1a532cc077be1
OrestBagan <orestbagan2233@gmail.com>
2023-02-01 22:30:49 +0200

    qwertyuiol;

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let dob = document.querySelector('#*');
+let dil = document.querySelector('#/');
+let plus = document.querySelector('#+');
+let minus = document.querySelector('#-');
+let rivne = document.querySelector('#=');
+
+let clear = document.querySelector('#C');
+
+let res = 0;
+document.querySelector('#res').innerHTML = res;
